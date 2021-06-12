c488a9f61ba007d6f24561c543d81997c7a3c199
lenmon <13422534160@163.com>
2021-06-12 19:00:01 +0800

    完成小视频模块

--- app/src/views/Video.jsx
+++ app/src/views/Video.jsx
@@ -1,77 +1,49 @@
-import React,{useState, useCallback,useRef,useEffect} from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { PullToRefresh, Button } from 'antd-mobile';
 import ReactDOM from 'react-dom';
 import '../scss/Video.scss'
-function Video(){
-    const [refreshing,seTrefreshing] = useState(false);
-    const [down,seTdown] = useState(true);
-    const [height,setHeight] = useState(document.documentElement.clientHeight)
-    const [data,setData] =useState([])
-    const [videoPlay,setVideoPlay] =useState(false);
-    const pull = useRef(null)
+function Video() {
+    const [videoPlay, setVideoPlay] = useState(false);
     const video = useRef(null)
-    const  getData  =()=> {
-        const dataArr = [];
-        for (let i = 0; i < 20; i++) {
-          dataArr.push(i);
-        }
-        return dataArr;
-      }
 
-      useEffect(()=>{
-        componentDidMount();
-      },[])
-
-   const  componentDidMount =()=>{
-        const hei = height - ReactDOM.findDOMNode(pull.current).offsetTop;
-        // console.log(pull);
-        
-    // console.log(ReactDOM.findDOMNode(pull.current));
-    
-        setTimeout(() => {
-            setHeight(hei),
-            setData(getData())
-        }, 0);
-      }
-    
-    return(
+    return (
         <div className="video_box">
-      <PullToRefresh
-        damping={60}
-        ref={pull}
-        style={{
-          height:height,
-          overflow: 'auto',
-          touchAction: 'none'
-        }}
-        indicator={down}
-        direction={down ? 'down' : 'up'}
-        refreshing={refreshing}
-        onRefresh={() => {
-          seTrefreshing(true)
-          setTimeout(() => {
-            seTrefreshing(false)
-          }, 1000);
-        }}
-      >
-        {/* {data.map(i => (
-          <div key={i} style={{ textAlign: 'center', padding: 20 }}>
-            {down ? 'pull down' : 'pull up'} {i}
-          </div>
-        ))} */}
-           <div className="content">
-                <div className="top_bar"></div>
-                <video src="https://vod.pipi.cn/d5457264vodtranscq1251246104/434f04463701925919031339538/v.f42905.mp4"  width="400" height="100%" loop="loop" ref={video} x5-video-orientation="portraint" controlslist="nodownload" onClick={()=>{
-                    if(videoPlay){
-                        video.current.play();
-                        setVideoPlay(false)
-                    }else{
-                        video.current.pause();
-                        setVideoPlay(true)
-                    }
-                }}></video>
-            </div>
-      </PullToRefresh>
+                <div className="video_main">
+                    <div className="shipin">
+                        <video src="https://vod.pipi.cn/d5457264vodtranscq1251246104/434f04463701925919031339538/v.f42905.mp4" width="400" height="100%" loop="loop" ref={video} x5-video-orientation="portraint" onClick={() => {
+                            if (!videoPlay) {
+                                video.current.play();
+                                setVideoPlay(true)
+                            } else {
+                                video.current.pause();
+                                setVideoPlay(false)
+                            }
+                        }}></video>
+                    {  videoPlay?<div></div>:<button className=" iconfont icon-icon_play"onClick={() => {
+                            if (!videoPlay) {
+                                video.current.play();
+                                setVideoPlay(true)
+                            } else {
+                                video.current.pause();
+                                setVideoPlay(false)
+                            }
+                        }}/>}
+                        <div className="tips">
+                            <i className='iconfont icon-gengduo'></i>
+                            <ul>
+                                <li> <img src="./img/adv2.jpg" alt="" /> <span>+</span></li>
+                                <li> <i className="iconfont icon-dianzan"></i> <p>52</p> </li>
+                                <li><i className="iconfont icon-53pinglun-"></i><p>评论</p> </li>
+                                <li><i className="iconfont icon-fenxiang"></i><p>分享</p> </li>
+                            </ul>
+                        </div>
+
+                        <div className="title">
+                            <span>@先声互动</span>
+                            <span>当你说我配不上你，我就把自己吃胖40斤！</span>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
